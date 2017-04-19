acad53b2b6f26b972cd2928e3bfde19ddc8b7515
Chris_Wallace <kawallis@icloud.com>
2017-04-19 16:16:47 -0700

    grayscale

--- test/bitmap-header.test.js
+++ test/bitmap-header.test.js
@@ -1,13 +1,17 @@
 const assert = require('assert');
 const constants = require('../lib/bitmap-constants');
 const BitmapHeader = require('../lib/bitmap-header');
+const fsp = require('fs-promise');
 
 describe('bitmap header', () => {
 
     let buffer = null;
     before(() => {
         // TODO: read './test/test-bitmap.bmp' into buffer variable
-
+        return fsp.readFile('./test/test-bitmap.bmp')
+        .then(data => {
+            buffer = data;
+        });
         // HINT: return a promise, this is async!
     });
 
