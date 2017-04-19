b4e5b9c285204708b679cafb6400cd4af270e2c6
Ivy Patton <lilaivy@gmail.com>
2017-04-19 13:46:46 -0700

    pixel offset defined

--- test/bitmap-header.test.js
+++ test/bitmap-header.test.js
@@ -11,7 +11,7 @@ describe('bitmap header', () => {
         // HINT: return a promise, this is async!
     });
 
-    it('has correct specs', () => {
+    it.only('has correct specs', () => {
         // : read the docs to figure out what these values should be
         assert.ok(constants.PIXEL_OFFSET);//
         assert.ok(constants.BITS_PER_PIXEL_OFFSET);//
