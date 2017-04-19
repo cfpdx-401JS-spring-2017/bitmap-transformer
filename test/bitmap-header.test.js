d8b884302eaaceaeb961d262030fc137f1dd4195
Morgan Fogarty <mofo37@gmail.com>
2017-04-19 15:35:35 -0700

    first assertion passes test

--- test/bitmap-header.test.js
+++ test/bitmap-header.test.js
@@ -1,29 +1,35 @@
 const assert = require('assert');
 const constants = require('../lib/bitmap-constants');
 const BitmapHeader = require('../lib/bitmap-header');
+const fsp = require('fs-promise');
 
 describe('bitmap header', () => {
 
-    let buffer = null;
-    before(() => {
+  let buffer = null;
+  before(() => {
+    return fsp.readFile('./test/test-bitmap.bmp')
+            .then(data => {
+                console.log('data', data);
+              buffer = data;
+            });
         // TODO: read './test/test-bitmap.bmp' into buffer variable
 
         // HINT: return a promise, this is async!
-    });
+  });
 
-    it('has correct specs', () => {
+  it('has correct specs', () => {
         // TODO: read the docs to figure out what these values should be
-        assert.ok(constants.PIXEL_OFFSET);
-        assert.ok(constants.BITS_PER_PIXEL_OFFSET);
-        assert.ok(constants.FILE_SIZE_OFFSET);
-    });
+    assert.ok(constants.PIXEL_OFFSET);
+    assert.ok(constants.BITS_PER_PIXEL_OFFSET);
+    assert.ok(constants.FILE_SIZE_OFFSET);
+  });
 
-    it('parses header data', () => {
+  it('parses header data', () => {
         // TODO: use the constants to populate the following properties
         // on the BitmapHeader in its constructor
-        const header = new BitmapHeader(buffer);
-        assert.equal(header.pixelOffset, 54);
-        assert.equal(header.bitsPerPixel, 24);
-        assert.equal(header.fileSize, 30054);
-    });
+    const header = new BitmapHeader(buffer);
+    assert.equal(header.pixelOffset, 54);
+    assert.equal(header.bitsPerPixel, 24);
+    assert.equal(header.fileSize, 30054);
+  });
 });
