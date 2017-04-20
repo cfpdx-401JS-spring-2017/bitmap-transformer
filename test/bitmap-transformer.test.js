fea5eaeb98ba93903334d0ad8e3defcdd4f89bf0
Ivy Patton <lilaivy@gmail.com>
2017-04-19 17:34:31 -0700

    passing final test for whole transfomr

--- test/bitmap-transformer.test.js
+++ test/bitmap-transformer.test.js
@@ -1,15 +1,20 @@
 const assert = require('assert');
 const fs = require('fs');
+const fsp = require('fs-promise');
 const BitmapTransformer = require('../lib/bitmap-transformer');
 const invert = require('../lib/invert-transformer');
 
 describe('bitmap file transformer', () => {
-    
+
     let buffer = null;
+
     before(() => {
         // TODO: read './test/test-bitmap.bmp' into buffer variable
         // HINT: return a promise, this is async!
-
+        return fsp.readFile('./test/test-bitmap.bmp')
+            .then(data => {
+                buffer = data;
+            });
         // TODO: the functionality in this before is same as 
         // other test, can you remove (extract) the duplication?
     });
@@ -29,8 +34,8 @@ describe('bitmap file transformer', () => {
 
         // read the output file we saved earlier as
         // the "standard" expected output file
-        const buffer = fs.readFileSync('./test/inverted-expected.bmp');
-        assert.deepEqual(bitmap.buffer, buffer);
+        const expectedBuffer = fs.readFileSync('./test/inverted-expected.bmp');
+        assert.deepEqual(bitmap.buffer, expectedBuffer);
 
         // if you don't have a standard file yet, you could write it 
         // out by commenting above code, using code below and visually inspect
